0ae0b0fa236a99b07f1e390fb52c68a426383fe9
Dinmukhamed Serik <lpi592753@gmail.com>
2024-07-19 00:27:33 +0300

    добавил решение третье

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем массив
+        int[] array = { 1, 2, 3, 4, 5, 6 };
+
+        // Выводим элементы массива, начиная с конца
+        Console.WriteLine("Элементы массива в обратном порядке:");
+        PrintArrayInReverse(array, array.Length - 1);
+    }
+
+    static void PrintArrayInReverse(int[] array, int index)
+    {
+        if (index < 0)
+        {
+            return;
+        }
+
+        Console.WriteLine(array[index]);
+        PrintArrayInReverse(array, index - 1);
+    }
+}
+
